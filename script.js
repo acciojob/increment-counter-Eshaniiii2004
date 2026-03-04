a59a98ff7a8a3543c80273723c5aa74ca3216595
Eshaniiii2004 <eshanighanwat2004@gmail.com>
2026-03-04 17:00:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counterElement=document.getElementById("counter");
+let button=document.getElementById("incrementBtn");
+button.addEventListener("click", function(){
+	let currentValue=parseInt(counterElement.innerText);
+	alert(currentValue);
+	counterElement.innerText=currentValue+1;
+})
